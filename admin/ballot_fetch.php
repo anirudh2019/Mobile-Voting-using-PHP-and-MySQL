2c2bcde10ff5c062436095531a1939be96c882e9
anirudh2019 <iit2019068@iiita.ac.in>
2021-05-01 14:04:59 +0530

    updated admin ballot position

--- admin/ballot_fetch.php
+++ admin/ballot_fetch.php
@@ -12,7 +12,7 @@
 	$query = $conn->query($sql);
 	$num = 1;
 	while($row = $query->fetch_assoc()){
-		$input = ($row['max_vote'] > 1) ? '<input type="checkbox" class="flat-red '.slugify($row['description']).'" name="'.slugify($row['description'])."[]".'">' : '<input type="radio" class="flat-red '.slugify($row['description']).'" name="'.slugify($row['description']).'">';
+		$input = '<input type="radio" class="flat-red '.slugify($row['wardname']).'" name="'.slugify($row['wardname']).'">';
 
 		$sql = "SELECT * FROM candidates WHERE position_id='".$row['id']."'";
 		$cquery = $conn->query($sql);
@@ -25,7 +25,7 @@
 			';
 		}
 
-		$instruct = ($row['max_vote'] > 1) ? 'You may select up to '.$row['max_vote'].' candidates' : 'Select only one candidate';
+		$instruct = 'Select only one candidate';
 		
 		$updisable = ($row['priority'] == 1) ? 'disabled' : '';
 		$downdisable = ($row['priority'] == $pquery->num_rows) ? 'disabled' : '';
@@ -35,7 +35,7 @@
 				<div class="col-xs-12">
 					<div class="box box-solid" id="'.$row['id'].'">
 						<div class="box-header with-border">
-							<h3 class="box-title"><b>'.$row['description'].'</b></h3>
+							<h3 class="box-title"><b>'.$row['wardname'].'</b></h3>
 							<div class="pull-right box-tools">
 				                <button type="button" class="btn btn-default btn-sm moveup" data-id="'.$row['id'].'" '.$updisable.'><i class="fa fa-arrow-up"></i> </button>
 				                <button type="button" class="btn btn-default btn-sm movedown" data-id="'.$row['id'].'" '.$downdisable.'><i class="fa fa-arrow-down"></i></button>
@@ -44,7 +44,7 @@
 						<div class="box-body">
 							<p>'.$instruct.'
 								<span class="pull-right">
-									<button type="button" class="btn btn-success btn-sm btn-flat reset" data-desc="'.slugify($row['description']).'"><i class="fa fa-refresh"></i> Reset</button>
+									<button type="button" class="btn btn-success btn-sm btn-flat reset" data-desc="'.slugify($row['wardname']).'"><i class="fa fa-refresh"></i> Reset</button>
 								</span>
 							</p>
 							<div id="candidate_list">
